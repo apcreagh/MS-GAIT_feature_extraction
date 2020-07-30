0770bd16805d43236915a47c0b83d4b7dd1fe989
Andrew Creagh <andrew.creagh@eng.ox.ac.uk>
2020-07-30 15:23:01 +0100

    Create dependencies.m

---
+++ dependencies.m
@@ -0,0 +1,15 @@
+%% Dependencies
+% There are a number of 3rd party software dependencies required I have
+% added a copy of the 3rd party software dependencies to the repository,
+% however, these can be manually added using the below code. This file will
+% download required files and folders that my toolbox relies The user may
+% wish to take this step manually. The author of this toolbox has no
+% responsibility for suggesting the downloading of third party software. If
+% you can't find these try googling them!
+unzip('http://www.maxlittle.net/software/fastdfa.zip', 'DFA')
+unzip('http://www.maxlittle.net/software/rpde.zip', 'RPDE')
+unzip('https://uk.mathworks.com/matlabcentral/fileexchange/14888-mutual-information-computation', 'MI')
+
+%Multi-Scale Entropy
+% visit:  'http://www.physionet.org/physiotools/mse/' and follow the
+% download instructions from there
